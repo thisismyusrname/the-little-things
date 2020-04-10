65a6e55e623f64d459e68d4250595bc664d8e486
Michael Costello <mpcostello2709@gmail.com>
2020-04-09 22:33:31 -0400

    trying to mimic the video. heaven help me.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,20 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>The Little Things</h1>
-    <p>I’m glad you’re here! Thank you for coming to take a look at The Little Things. I know you have looked forward to your upcoming wedding, shower, or event for many years. I also know that you probably already have a good idea of what you want your special day to look like. I would love to help turn your ideas into a reality.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+  <div>
+    <div className="LandingView">
+      <div className="HeroGroup">
+       <h1>The Little Things</h1>
+       <p>I’m glad you’re here! Thank you for coming to take a look at The Little Things. I know you have looked forward to your upcoming wedding, shower, or event for many years. I also know that you probably already have a good idea of what you want your special day to look like. I would love to help turn your ideas into a reality.</p>
     <Link to="/page-2/">View Categories</Link>
-  </Layout>
+      </div>
+    </div>
+    <SEO title="Home" />
+
+  </div>
 )
 
 export default IndexPage
