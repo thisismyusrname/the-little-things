827216c1f3bb78468720076ace949dd499bf4854
Michael Costello <mpcostello2709@gmail.com>
2020-04-09 22:59:30 -0400

    moved css and index.js into "layout"

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -6,7 +6,7 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <div>
     <div className="LandingView">
-      <div className="HeroGroup">
+      <div className="LandingContent">
        <h1>The Little Things</h1>
        <p>I’m glad you’re here! Thank you for coming to take a look at The Little Things. I know you have looked forward to your upcoming wedding, shower, or event for many years. I also know that you probably already have a good idea of what you want your special day to look like. I would love to help turn your ideas into a reality.</p>
     <Link to="/page-2/">View Categories</Link>
